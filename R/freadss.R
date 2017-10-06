e4719a069d3b3359e097112cab95df0a96f5ac4a
mikejacktzen <mtzen@lexis.ccpr.ucla.edu>
2017-10-05 17:43:39 -0700

    removed optional args in freadss()
    book keeping original header names is complex

--- R/freadss.R
+++ R/freadss.R
@@ -2,63 +2,71 @@
 #'
 #' @seealso \code{\link[data.table]{fread}}
 #' @description The input file is read in-memory via \code{\link[data.table]{fread}}.
-#' If rows are subset, there is a speed down. Hence, subsetting rows takes on a one-time slow down
-#' in order to bean count the read-in memory footprint
+#' If rows are subset, there is a slow down. Hence, subsetting rows costs a one-time slow down
+#' but affords ability to bean count the read-in memory footprint
+#'
+#' @details NOTE: if both ss and ind_choose are NULL, no subsetting is done. Entire csv is read in.
+#'
 #' @param input character for file path of csv passed to \code{\link[data.table]{fread}}
 #' @param ss integer for desired row sample size. Default of ss is NULL, meaning no subsampling.
 #' @param replace a logical picking with/without-replacement sampling
 #' @param ind_choose optional integer vector of specific rows to read in (instead of sampling)
-#' @param ... optional args passed to \code{\link[data.table]{fread}}
 #'
 #' @return a 'data.frame' with optionally subsetted rows (perhaps from sampling)
 #' @export
 #'
 #' @examples
 #'
-#' set.seed(1)
-#' m = matrix(rnorm(1e5),ncol=10)
-#' csv = data.frame(x=1:1e4,m)
+#' set.seed(1); m = matrix(rnorm(10*100),ncol=100,nrow=100)
 #'
-#' setwd("~/projects/datzen/tests/proto/")
-#' write.csv(csv,"test.csv")
+#' csv = data.frame(m)
+#' names(csv) = paste0('x',seq_along(csv))
+#' names(csv)
 #'
-#' dir_test = '~/projects/datzen/tests/proto/test.csv'
+#' tf = tempfile()
+#' write.csv(csv,tf,row.names=FALSE)
+#' dir_test=tf
 #'
-#' result = freadss(input=dir_test,ss=1000,replace=TRUE,ind_choose=NULL)
-#' # sometimes sample drops some row
+#' # if ss=NULL and ind_choose=NULL
+#' # no sub sampling, basically fread() but no flexible optional args.
+#' # just demo, might as well use fread() directly
 #'
-#' dim(result)
-#' head(result)
+#' identical(freadss(input=dir_test),fread(dir_test))
 #'
-#' ind_pick = as.integer(50:100)
-#' result = freadss(input=dir_test,replace=TRUE,
-#'                  ind_choose=ind_pick,
-#'                  drop='V12')
+#' # user wants to sample 5 random rows
+#' freadss(input=dir_test,ss=5)
 #'
-#' identical(sort(as.integer(result$V1)),sort(ind_pick))
+#' # user picks 5 specific rows
+#' ind_pick = c(1,7,23,69,100)
 #'
-#' dim(result)
-#' head(result)
-#' tail(result)
+#' df_subset_before = freadss(input=dir_test,ind_choose = ind_pick)
+#' df_subset_after = freadss(input=dir_test)[ind_pick,]
+#' identical(df_subset_before,df_subset_after)
 #'
-#' # no sub sampling
-#' result = freadss(input=dir_test,ss=NULL)
-#' dim(result)
 
-freadss = function(input,ss=NULL,replace=TRUE,ind_choose=NULL,...){
+
+# disabled optional ... args passed to fread()
+# eg drop/keep bugs out since issue of original header name lost during do.call(fread,list_ss)
+
+freadss = function(input,ss=NULL,replace=TRUE,ind_choose=NULL){
 
   # ss = 100  # samp size
-  stopifnot(is.null(ss)||(ss>0))
+  # ind_choose=ind_pick
+
+  # negative ss
+
+  # ss and ind_choose non null
+  if((!is.null(ss))&&(!is.null(ind_choose))){stop('you can not have BOTH non-null ss and ind_choose')}
+
 
   require(data.table)
 
   # other args passed to fread()
-  dots = list(...)
+  # dots = list(...)
 
-  if(is.null(ss)==TRUE){
-    args_all = append(input,dots)
+  if((is.null(ss)&&is.null(ind_choose))==TRUE){
     # no subsampling of rows
-    return(do.call(fread,args_all))
+    return(fread(input))
   }
 
   # else, subsample rows
@@ -67,8 +75,13 @@ freadss = function(input,ss=NULL,replace=TRUE,ind_choose=NULL,...){
   # input = '~/projects/datzen/tests/proto/test.csv'
 
   num_rows = data.table::fread(paste0('wc -l ',input))[[1]] - 1
+  name_header_orig = names(fread(input,nrows=0))
 
-  if(is.null(ind_choose)){
+  # row index random sampled
+  if(is.null(ind_choose)&&(!is.null(ss))){
+
+    if(ss <= 0){stop('ss must be NULL or greater than 0')}
+
     # use ind_samp
     if(num_rows < ss){
       ind_samp = sample(x=(1:nrow(dat_raw)),size=ss,replace=TRUE)
@@ -78,8 +91,10 @@ freadss = function(input,ss=NULL,replace=TRUE,ind_choose=NULL,...){
       ind_samp = sample(x=(1:num_rows),size=ss,replace=replace)
     }
     ind_spec = as.integer(ind_samp)
-  } else {
-    # ind_choose = c(1:50,53, 65,77,90,100:200,350:500, 5000:6000)
+  }
+
+  # row index user specified
+  if(!is.null(ind_choose)&&(is.null(ss))){
     ind_spec = as.integer(ind_choose)
   }
 
@@ -88,47 +103,60 @@ freadss = function(input,ss=NULL,replace=TRUE,ind_choose=NULL,...){
   v = rep(FALSE,num_rows)
   v[ind_spec] = TRUE
   # sum(v)
+  # v <- (1:num_rows %in% ind_spec)
 
   seq  = rle(v)
   idx  = c(0, cumsum(seq$lengths))[which(seq$values)] + 1
-
-  df_indx = (data.frame(start=as.integer(idx),
-                        length=as.integer(seq$length[which(seq$values)])
-                        ))
-
+  df_indx = data.frame(start=idx, length=seq$length[which(seq$values)])
   # str(df_indx)
 
-  result = do.call(rbind,
-                   apply(X=df_indx,MARGIN=1,
-                         FUN=function(xx){
+  result = do.call(rbind,apply(df_indx,1, function(x) return(fread(input,nrows=x[2],skip=x[1]))))
 
-                           # str(df_indx)
-                           # xx = df_indx[1,]
+  names(result) = name_header_orig
 
-
-
-                           args_cust = list(input=input,
-                                            nrows=unlist(xx[2]),
-                                            skip=unlist(xx[1]))
-
-                           # str(args_cust)
-                           # append 'dots' from topmost scope
-
-                           args_all = append(args_cust,dots)
-                           # do.call(fread,args_all)
-
-                           return(do.call(fread,args_all))
-
-                         }))
+  # revisit drop keep optional args
+  # names(result) = name_header_orig[!(name_header_orig %in% drop)]
 
   return(result)
 }
 
 
 
-
-
 ########################################
+#
+#
+# result = do.call(rbind,
+#                  apply(X=df_indx,MARGIN=1,
+#                        FUN=function(xx){
+#
+#                          # str(df_indx)
+#                          # xx = df_indx[1,]
+#
+#
+#                          # internal do.call(fread,X[[i]])
+#                          # will chop off first global row (true header)
+#                          # and following rows will be auto renamed to use V1 etc
+#                          # behavior of fread()
+#
+#                          args_cust = list(input,
+#                                           # header=FALSE,
+#                                           nrows=unlist(xx[2]),
+#                                           skip=unlist(xx[1])
+#                          )
+#
+#                          # str(args_cust)
+#
+#                          # append 'dots' from topmost scope
+#                          # args_all = append(args_cust,dots)
+#
+#                          args_all = args_cust
+#
+#
+#                          return(do.call(fread,args_all))
+#
+#                        }))
+#
+#
 # one line at time is signifig slower
 #
 #
