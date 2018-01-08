22054270c6cd20d7424dbe0a68f9c82dc7adaaba
mikejacktzen <mtzen@lexis.ccpr.ucla.edu>
2018-01-08 10:20:12 -0800

    simplified colname_zen
    rlang and etc was too bloated

---
+++ R/colname_zen.R
@@ -0,0 +1,72 @@
+
+#' the colname_zen() function zenifies a data frame's column names by lower and caterpillar casing
+#'
+#' @param df a data.frame
+#' @param suffix a single character string (optional) to attach as a suffix to column names (default NULL)
+#' @param ind_suffix an integer vector (optional) that picks the specific columns to attach the suffix (default NULL)
+#'
+#' @return a modified character vector of names that have been lower and caterpillar cased with an optional suffix
+#' @export
+#'
+#' @examples
+#' df_foo = iris
+#'
+#' df_foo %>% colname_zen()
+#' df_foo %>% colname_zen(.,suffix="src_1")
+#' df_foo %>% colname_zen(.,suffix="src_1",c(1,5))
+#'
+#' # assign names to original data frame
+#' names_new = df_foo %>% colname_zen(.,suffix="src_1",c(1,5))
+#' names(df_foo) = names_new
+#' names(df_foo)
+
+colname_zen = function(df,
+                       suffix=NULL,
+                       ind_suffix=NULL){
+
+  require(dplyr)
+  require(stringr)
+  # names(df)
+  # suffix = NULL
+  # suffix = 'src1'
+
+  if(!is.null(suffix)){
+    suffix_fin = paste0("_",suffix)
+  } else {
+    suffix_fin = NULL
+  }
+  # specific column index for suffix
+  if(is.null(ind_suffix)){
+    ind_suffix = seq_along(df)
+    # else user supplied integer vector
+  }
+  if(any(!(ind_suffix %in% seq_along(df)))){
+    stop('A column index you specified in "ind_suffix" is outside the range of seq_along(df)')
+  }
+  names_raw = names(df)
+  names_case = names_raw %>%
+    str_replace_all(.,pattern="\\.",replacement="_") %>%
+    str_replace_all(.,pattern=" ",replacement="_") %>%
+    tolower()
+  # ind_suffix = c(1,4)
+  names_new = paste0(names_case[ind_suffix],
+                     suffix_fin)
+  names_case[ind_suffix] = names_new
+  return(names_case)
+
+}
+
+
+
+
+# df_foo = iris
+#
+# df_foo %>% colname_zen()
+# df_foo %>% colname_zen(.,suffix="src_1")
+# df_foo %>% colname_zen(.,suffix="src_1",c(1,5))
+#
+# names_new = df_foo %>% colname_zen(.,suffix="src_1",c(1,5))
+#
+# # assign names to original data frame
+# names(df_foo) = names_new
+# names(df_foo)
--- man_md/colname_zen.md
+++ man_md/colname_zen.md
@@ -4,12 +4,15 @@
 
 
  the colname_zen() function zenifies a data frame's column names by lower and caterpillar casing
+ 
+ the colname_zen() function zenifies a data frame's column names by lower and caterpillar casing
 
 
 ## Usage
 
 ```r
-colname_zen(df, suffix = NULL)
+colname_zen(df, suffix = NULL, ind_suffix = NULL)
+colname_zen(df, suffix = NULL, ind_suffix = NULL)
 ```
 
 
@@ -19,11 +22,16 @@ Argument      |Description
 ------------- |----------------
 ```df```     |     a data.frame
 ```suffix```     |     a single character string to attach as a suffix to column names
+```ind_suffix```     |     an integer vector (optional) that picks the specific columns to attach the suffix (default NULL)
+```df```     |     a data.frame
+```suffix```     |     a single character string (optional) to attach as a suffix to column names (default NULL)
 
 ## Value
 
 
  a data.frame whose column names have been lower and caterpillar cased
+ 
+ a modified character vector of names that have been lower and caterpillar cased with an optional suffix
 
 
 ## Examples
@@ -32,5 +40,15 @@ Argument      |Description
  iris %>% colname_zen() %>% head
  
  iris %>% colname_zen(.,suffix="src_1") %>% head
+ df_foo = iris
+ 
+ df_foo %>% colname_zen()
+ df_foo %>% colname_zen(.,suffix="src_1")
+ df_foo %>% colname_zen(.,suffix="src_1",c(1,5))
+ 
+ # assign names to original data frame
+ names_new = df_foo %>% colname_zen(.,suffix="src_1",c(1,5))
+ names(df_foo) = names_new
+ names(df_foo)
  ``` 
 
