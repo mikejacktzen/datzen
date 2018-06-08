60349506f412cbe7635a9eb361957edf5af590c7
mikejacktzen <mtzen@lexis.ccpr.ucla.edu>
2018-06-08 16:46:04 -0700

    got rid of dplyr in load_cache()

--- R/load_cache.R
+++ R/load_cache.R
@@ -13,8 +13,8 @@
 #' dir_cache = here::here("/writeup/article_tech_doc_cache/html/")
 #' load_cache(dir_cache)
 load_cache = function(dir_cache,envir=globalenv(),...){
-  files_rdata = list.files(dir_cache,pattern='.RData') %>%
-    gsub(.,pattern=".RData",replacement="")
+
+  files_rdata = gsub(list.files(dir_cache,pattern='.RData'),pattern=".RData",replacement="")
 
   # files_rdata
   for(i in seq_along(files_rdata)){
